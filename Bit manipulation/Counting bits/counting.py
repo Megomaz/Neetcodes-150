5573a2f14b6b1371f698502ab79a5f851fb45b91
Spencer Kesse <137727636+Megomaz@users.noreply.github.com>
2025-06-29 14:50:12 +0000

    Count bits

---
+++ Bit manipulation/Counting bits/counting.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        result = []
+
+        for i in range(n+1):
+            count = 0
+            while i:
+                count += i & 1
+                i >>= 1
+            result.append(count)
+
+        return result
+
+
+# another solution (not mine)
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [0] * (n + 1)
+        for i in range(n + 1):
+            dp[i] = dp[i >> 1] + (i & 1)
+        return dp
