f467524694bd7049dbcaac6254066aa0a501e442
Spencer Kesse <137727636+Megomaz@users.noreply.github.com>
2025-07-02 17:57:46 +0000

    Set matrix

---
+++ Math & geometry/Set zereos/set.py
@@ -0,0 +1,46 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                if matrix[r][c] == 0:
+                    for i in range(len(matrix[r])):
+                        if matrix[r][i] != 0:
+                            matrix[r][i] = 'T'
+                    for x in range(len(matrix)):
+                        if matrix[x][c] != 0:
+                            matrix[x][c] = 'T'
+
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                if matrix[r][c] == 'T':
+                    matrix[r][c] = 0
+        
+
+# better solution (not mine)
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        ROWS, COLS = len(matrix), len(matrix[0])
+        rowZero = False
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else:
+                        rowZero = True
+
+        for r in range(1, ROWS):
+            for c in range(1, COLS):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+
+        if matrix[0][0] == 0:
+            for r in range(ROWS):
+                matrix[r][0] = 0
+
+        if rowZero:
+            for c in range(COLS):
+                matrix[0][c] = 0
