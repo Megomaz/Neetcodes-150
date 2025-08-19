01700896c9a016895ac9b2985c271afd36add9dc
Spencer Kesse <137727636+Megomaz@users.noreply.github.com>
2025-08-19 12:53:24 +0000

    Count Good Nodes

---
+++ Trees/Count Good Nodes/good.py
@@ -0,0 +1,46 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        self.good = 0
+
+        def dfs(root,Max):
+            if not root:
+                return 
+            
+            if root.val >= Max:
+                Max = root.val
+                self.good += 1
+
+            dfs(root.left, Max)
+            dfs(root.right, Max)
+
+        dfs(root,-101)
+        return self.good
+    
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+
+        def dfs(node, maxVal):
+            if not node:
+                return 0
+
+            res = 1 if node.val >= maxVal else 0
+            maxVal = max(maxVal, node.val)
+            res += dfs(node.left, maxVal)
+            res += dfs(node.right, maxVal)
+            return res
+
+        return dfs(root, root.val)
